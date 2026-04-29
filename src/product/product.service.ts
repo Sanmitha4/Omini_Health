7902608a20bd33730f842b2f2f4a0903994225b2
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-04-29 11:37:01 +0000

    Installed swagger

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
